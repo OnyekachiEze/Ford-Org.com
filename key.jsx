2a43ca4cc26dba157ec4e54ae7b3ccfa7be95e1a
OnyekachiEze <ezeonyekachipaul@gmail.com>
2024-05-21 15:29:44 +0100

    first commit

---
+++ key.jsx
@@ -0,0 +1,12 @@
+let y = document.querySelector('.media');
+let x = document.querySelector('#search')
+
+
+function slide(){
+y.classList.toggle('slideout')
+}
+let a = 2
+function search(){
+    // document.getElementById('search').style.display = 'inline-flex'
+    x.classList.toggle('slideout2')
+}
